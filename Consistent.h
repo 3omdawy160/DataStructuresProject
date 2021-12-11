87c5fd010818276f4ce2aa85b56a61b11c0d29c3
Mohamed Emad El-Dien Mahmoud <85120725+3omdawy160@users.noreply.github.com>
2021-12-11 16:51:53 +0200

    Create Consistent.h

---
+++ Consistent.h
@@ -0,0 +1,9 @@
+#ifndef DATASTRUCTURESPROJECT_H_
+#define DATASTRUCTURESPROJECT_H_
+#include <iostream>
+#include<string>
+#include <stack>
+using namespace std;
+
+
+#endif /* DATASTRUCTURESPROJECT_H_ */
