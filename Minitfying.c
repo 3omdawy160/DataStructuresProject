16834a5a2a17c9949ef8c5dc53aeb5375e37ff41
Mohamed Emad El-Dien Mahmoud <85120725+3omdawy160@users.noreply.github.com>
2021-12-14 03:18:28 +0200

    Update Minitfying.c

--- Minitfying.c
+++ Minitfying.c
@@ -1,13 +1,15 @@
 #include "header.h"
 
+// This function aims to take a string with new lines,tabs and spaces and convert it into a single line string.
+
 string Minifying(string s)
 {
-	for(int i=0;i<s.length();i++)
+	for(int i=0;i<s.length();i++)   // Looping on the whole string char by char
 	{
-		if(s[i]=='\n' || s[i]==' ')
+		if(s[i]=='\n' || s[i]==' ') // In case we found a new line or a space in the string
 		{
-			s.erase(i,1);
-			i--;
+			s.erase(i,1); // Erasing this space or new line
+			i--;          // Decreminting the iterator i to as we erased a place therefore i must be Decreminted to be at the same place it were 
 		}
 	}
 
