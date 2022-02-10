71903998425f5c1d882bcccd4f2dea004d6c582d
Mohamed Emad El-Dien Mahmoud <85120725+3omdawy160@users.noreply.github.com>
2022-02-10 17:26:35 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,9 +1,10 @@
 #ifndef DATASTRUCTURESPROJECT_H_
 #define DATASTRUCTURESPROJECT_H_
+
 #include <iostream>
 #include<string>
 #include <stack>
+#include <vector>
 using namespace std;
-
 
 #endif /* DATASTRUCTURESPROJECT_H_ */
